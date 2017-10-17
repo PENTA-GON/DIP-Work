66a570cd2da637108aac93e5faf1b28461ff107e
PENTA-GON <32325746+PENTA-GON@users.noreply.github.com>
2017-10-17 13:02:17 +0800

    Update colorIndexing.m

--- Color Indexing/colorIndexing.m
+++ Color Indexing/colorIndexing.m
@@ -1,15 +1,20 @@
 clear; close all; clc;
 
 
-I = imread('69.jpg');
+I = imread('.\Training\38.jpg');
+
+figure('NumberTitle', 'off', 'Name', '3D and 2D color histogram');
+set(gcf, 'position', [100 100 1200 500]);
 
 % 3D histogram
-nbins = [16 8 16];
-flag = 0;
+nbins3d = [16 8 16];
+flag3d = 0;
+subplot(1,2,1),histogram3d2d( I, nbins3d,flag3d);
 
 % 2D histogram 
-%nbins = [8 8];
-%flag = 1;
+nbins2d = [8 8];
+flag2d = 1;
+subplot(1,2,2),histogram3d2d( I, nbins2d,flag2d);
 
-histogram3d2d( I, nbins,flag);
+
 
