c4ae9a173006cec085701c340facdf3a02eeac3b
PENTA-GON <32325746+PENTA-GON@users.noreply.github.com>
2017-10-17 16:56:00 +0800

    Updating colorindexing.m file

--- Color Indexing/colorIndexing.m
+++ Color Indexing/colorIndexing.m
@@ -9,16 +9,11 @@ set(gcf, 'position', [100 100 1200 500]);
 
 
 % 3D histogram
-nbins = [16 8 16];
-flag = 0;
 nbins3d = [16 8 16];
 flag3d = 0;
 subplot(1,2,1),histogram3d2d( I, nbins3d,flag3d);
 
 % 2D histogram 
-% nbins = [8 8];
-% flag = 1;
-=======
 nbins2d = [8 8];
 flag2d = 1;
 subplot(1,2,2),histogram3d2d( I, nbins2d,flag2d);
