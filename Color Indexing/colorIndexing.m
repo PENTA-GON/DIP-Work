721beee34e5f405ae084013f6dfbcb25ea5aeffc
Aung Aung Phyo Wai <aung2phyowai@gmail.com>
2017-10-19 10:38:02 +0800

    Minor change to add image display and add 4th parameter to control histogram plotting

--- Color Indexing/colorIndexing.m
+++ Color Indexing/colorIndexing.m
@@ -2,16 +2,18 @@ clear; close all; clc;
 
 I = imread('.\PP_Toys_03\Training\38.jpg');
 
+figure;imshow(I);
+
 figure('NumberTitle', 'off', 'Name', '3D and 2D color histogram');
 set(gcf, 'position', [100 100 1200 500]);
 
-
+isPlot = true;
 % 3D histogram
 nbins3d = [16 8 16];
 flag3d = 0;
-subplot(1,2,1),histogram3d2d( I, nbins3d,flag3d);
+subplot(1,2,1),histogram3d2d( I, nbins3d,flag3d,isPlot);
 
 % 2D histogram 
 nbins2d = [8 8];
 flag2d = 1;
-subplot(1,2,2),histogram3d2d( I, nbins2d,flag2d);
+subplot(1,2,2),histogram3d2d( I, nbins2d,flag2d,isPlot);
