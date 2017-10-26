cd2a20d81b590bb5e2f490c8b9b480dfe73ca7af
DAIWENTING <32350664+DAIWENTING@users.noreply.github.com>
2017-10-26 19:46:52 +0800

    Update colorIndexing.m

--- Color Indexing/colorIndexing.m
+++ Color Indexing/colorIndexing.m
@@ -10,16 +10,7 @@ clear; close all; clc;
 %M1:69 M2:83 M3:100 I:147
 %M1:0,1(error) M2:4,6(error) M3:28 I:129
 
-%using 2d histogram
-%M1:96 M2:14 M3:177 I:195
-%M1:34 M2:44 M3:92 I:164
 
-%M1:78(error) M2:92 M3:65 I:133
-%M1:53 M2:63(error) M3:75 I:190 #nbins
-%M1:86 M2:93 M3:106(error) I:150
-%M1:75(error) M2:53 M3:27 I:142
-%M1:69 M2:83(error) M3:100(error) I:147
-%M1:0,1 M2:4,6(error) M3:28 I:129
 M1 = imread('.\PP_Toys_03\Training\69.jpg');
 M2 = imread('.\PP_Toys_03\Training\83.jpg');
 M3 = imread('.\PP_Toys_03\Training\171.jpg');
