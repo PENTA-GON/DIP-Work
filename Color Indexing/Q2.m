c017f7015db1f6ce90cad6451ab5ada485c45ac5
Aung Aung Phyo Wai <aung2phyowai@gmail.com>
2017-10-27 17:29:14 +0800

    Changes to visualize 3 figures for - histogram intersection (3d) - histogram intersection (2d) - Incremental histogram intersection (3d)

--- Color Indexing/Q2.m
+++ Color Indexing/Q2.m
@@ -2,28 +2,31 @@ clear;close all;clc;
 
 %% To visualize similarity among training data sets
 train_folder_name = '.\PP_Toys_03\Training';
-% all files in training folder
+% Get all files in training folder
 filenames=dir(fullfile(train_folder_name,'*.jpg'));
 filenames={filenames.name};
 n_samples=numel(filenames);
 
-%% Visualization of 3D and 2D plot
-nbins3d = [16 8 16];
-nbins2d = [8 8];
+% Specifying parameters of 3D and 2D histograms
+nbins3d = [16 8 16]; % # bins per color
 flag3d = 0;
+nbins2d = [8 8]; % # bins per color
 flag2d = 1;
-
-%% Basic Histogram Intersection
-isPlot = false;
+plotScale = 100;
+%% Histogram creations using opponent color spaces
+isPlot = false; %No histogram plotting
 for i=1:n_samples 
     I = imread(fullfile(train_folder_name, num2str(cell2mat(filenames(i)))));
     [hist3d{i}, bin_3d] = histogram3d2d( I, nbins3d,flag3d, isPlot);
     [hist2d{i}, bin_2d] = histogram3d2d( I, nbins2d,flag2d, isPlot);
 end
-save('hist3d.mat', 'hist3d');
-save('hist2d.mat', 'hist2d');
+%saving histogram intersection results into files
+%save('hist3d.mat', 'hist3d');
+%save('hist2d.mat', 'hist2d');
 
-load('hist3d.mat');
+%% Basic Histogram Intersection (3d histogram)
+%loading previously saved 3d histogram results
+%load('hist3d.mat');
 nImg = numel(hist3d);
 sResults3d = zeros(nImg, nImg);
 for i= 1 : nImg
@@ -31,9 +34,15 @@ for i= 1 : nImg
         sResults3d(i,j) = HistIntersec_3D(hist3d{i}, hist3d{j});
     end
 end
-save('histResults3d.mat', 'sResults3d');
-
-load('hist2d.mat');
+%save('histResults3d.mat', 'sResults3d');
+% Plot histogram intersection results similar to Fig 7
+ShowHistIntersectResults(sResults3d, plotScale);
+set(gcf, 'NumberTitle', 'off');
+set(gcf, 'Name', 'Histogram Intersection using 3D Histogram');
+title('Histogram Intersection using 3D Histogram');
+    
+%% Basic Histogram Intersection (2d histogram)
+%load('hist2d.mat');
 nImg = numel(hist2d);
 sResults2d = zeros(nImg, nImg);
 for i= 1 : nImg
@@ -41,27 +50,27 @@ for i= 1 : nImg
         sResults2d(i,j) = HistIntersec(hist2d{i}, hist2d{j});
     end
 end
-save('histResults2d.mat', 'sResults2d');
-
-% To plot histogram intersection results similar to Fig 7
-ShowHistIntersectResults(sResults3d, 100);
-ShowHistIntersectResults(sResults2d, 100);
+%save('histResults2d.mat', 'sResults2d');
+% Plot histogram intersection results similar to Fig 7
+ShowHistIntersectResults(sResults2d, plotScale);
+set(gcf, 'NumberTitle', 'off');
+set(gcf, 'Name', 'Histogram Intersection using 2D Histogram');
+title('Histogram Intersection using 2D Histogram');
 
 %% Incremental Histogram Intersection
-load('hist3d.mat');
+%load('hist3d.mat');
 nImg = numel(hist3d);
-% off-line building of database
-
+%% Step-1: off-line building of database
 isPlot = false;
 max_idx_hist = cell(1,75);
 %inc_hist_model = cell(1,75);
 for i= 1 : nImg
     [max_idx_hist{i}] = BuildHistIncrementIntersect(hist3d{i}, nbins3d);
 end
-save('max_idx_hist.mat','max_idx_hist');
+%save('max_idx_hist.mat','max_idx_hist');
 
-%online matching of test image
-load('max_idx_hist.mat');
+%% Setp-2: online matching of test image with database
+%load('max_idx_hist.mat');
 maxBins = 10;
 sMatch = zeros(nImg, nImg);
 for i= 1 : nImg
@@ -72,5 +81,8 @@ for i= 1 : nImg
     %% 2. Perform histogram matching
    sMatch(i,:) = MatchIncreHistIntersect(max_idx_hist, sorted_test_hist, color_bins);
 end
-
-ShowHistIntersectResults(sMatch, 100);
+% Plot incremental histogram intersection results
+ShowHistIntersectResults(sMatch, plotScale);
+set(gcf, 'NumberTitle', 'off');
+set(gcf, 'Name', 'Incremental Histogram Intersection using 3D Histogram');
+title('Incremental Histogram Intersection using 3D Histogram');
