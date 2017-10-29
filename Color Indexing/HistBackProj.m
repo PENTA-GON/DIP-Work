47a0440ed5d480fbac8eff527086792395245f2a
PENTA-GON <32325746+PENTA-GON@users.noreply.github.com>
2017-10-29 21:35:22 +0800

    Update HistBackProj.m

--- Color Indexing/HistBackProj.m
+++ Color Indexing/HistBackProj.m
@@ -4,46 +4,39 @@ function [ ind_x,ind_y ] = HistBackProj( M, I ,r)
 
 [m,n,~] = size(I);
 
-nbins3d = [16 8 16];
-
+nbins = [16 8 16];
 flag = 0;
-
 isPlot = 0;
 
-%figure;
-%subplot(1,2,1),
-[hist_M,bin_M] = histogram3d2d( M,nbins3d,flag, isPlot );
-%subplot(1,2,2),
-[hist_I,bin_I] = histogram3d2d( I,nbins3d,flag, isPlot );
+
+[hist_M,bin_M] = histogram3d2d( M,nbins,flag, isPlot );
+[hist_I,bin_I] = histogram3d2d( I,nbins,flag, isPlot );
 
 
-%Ratio histogram
+%ratio histogram
 hist_R = min(hist_M ./ (hist_I) , 1);
 
 B = zeros(size(bin_I,1),1);
 
+
 for i = 1:size(bin_I,1) 
     
+    %record the bin label of each color channel for i-th pixel 
     a = bin_I(i,1);
     b = bin_I(i,2);
     c = bin_I(i,3);
+    
+    %map the ratio histogram value for each pixel
     B(i) = hist_R(a,b,c);
-    %{
-    if(flag == 0)
-        c = bin_I(i,3);
-        B(i) = hist_R(a,b,c);
-    else
-        B(i) = hist_R(a,b,c);
-    end
-    %}
+ 
 end
 B = reshape(B,[m,n]);
-%imshow(B);
 
-%disk filter
+%define the disk filter
 f = zeros(2*r,2*r);
 
-for i = 1: r
+%one quater of the disk
+for i = 1: r 
     for j = 1: r
         if((i-r)^2 + (j-r)^2 < r^2)
             f(i,j) = 1;
@@ -55,12 +48,11 @@ f = f + f(: ,end :-1 :1);% Mirror horizontally
 f = f + f(end:-1:1,:);% Mirror vertically          
 
 B1 = conv2(B,f,'same');
-c = max(B1(:));
-%imshow(B1/c);
 
 [~,ind] = max(B1(:));
 [ind_x, ind_y] = ind2sub([m n],ind);
 
+%plot 
 theta = linspace(0,2*pi);
 x = r*cos(theta) + ind_x;
 y = r*sin(theta) + ind_y;
@@ -72,14 +64,6 @@ subplot(1,2,2),imshow(I),hold on; title('The image');
 
 plot(ind_y,ind_x,'s','color','red');
 plot(y,x,'color','green');
-
-
-
-
-              
-    
-
-
 
 end
 
