47c91162c0d47b820e54713420cbfd0902d672e6
PENTA-GON <32325746+PENTA-GON@users.noreply.github.com>
2017-10-29 21:26:50 +0800

    Update histogram3d2d_NoScaling.m

--- Color Indexing/histogram3d2d_NoScaling.m
+++ Color Indexing/histogram3d2d_NoScaling.m
@@ -1,4 +1,4 @@
-function [ n, bin ] = histogram3d2d_NoScaling( I,nbins,flag, isPlot )
+function [ n1,bin ] = histogram3d2d( I,nbins,flag, isPlot )
 % Task1.Create 3D and 2D histogram
 % I: image input; nbins: size of bins; flag: 0-3D histogram and 1-2D histogram
 
@@ -18,8 +18,12 @@ if (flag == 0)
 else 
     r1 = r ./ (r + g + b);
     g1 = g ./(r + g + b);
+    w = [r1(:),g1(:)];
     
-   w = [r1(:),g1(:)];
+   %Adding b channel will improve the intersection result 
+   %b1 = 1 - r1 - g1;
+   %w = [r1(:),g1(:),b1(:)];
+   %nbins = [nbins,nbins(1)];
 end
 
 [nrows,ncols] = size(w);
@@ -27,18 +31,40 @@ end
 bin = zeros(nrows,ncols);
 
 for i = 1:ncols
-    minx = min(w(:,i));
-    maxx = max(w(:,i));
+    %minx = min(w(:,i));
+    %maxx = max(w(:,i));
+    
+    %set the range of each axis
+    if(flag == 0)
+        if(ncols == 3 && i == 2)%for wb axis
+            minx = 0 ;
+            maxx = 255 * 3  * 0.65;
+            
+        elseif(ncols == 3 && i == 3)%for by axis
+            minx = -255 * 2  * 0.5  ;
+            maxx = 255 * 2 * 0.5 ;
+            
+        else   %for rg axis
+            minx = -255  * 0.5 ;
+            maxx = 255 * 0.5 ;
+        end
+    else 
+        minx = 0;
+        maxx = 1.0;
+    end
+ 
     
     % Make histc mimic hist behavior  
     binwidth{i} = (maxx - minx) / nbins(i);
-    edges{i} = minx + binwidth{i}*(0:nbins(i));
-    ctrs{i} = edges{i}(1:nbins(i)) + binwidth{i}/2;
+    edges{i} = minx + binwidth{i}*(0:nbins(i));%range of each bin
+    ctrs{i} = edges{i}(1:nbins(i)) + binwidth{i}/2;%centers of bins
 
-    % everything < ctrs(1) gets counted in first bin, 
-    % everything > ctrs(end) gets counted in last bin.
+    % everything < edges(1) gets counted in first bin, 
+    % everything > edges(end) gets counted in last bin.
     histcEdges = [-Inf edges{i}(2:end-1) Inf];
     [dum,bin(:,i)] = histc(w(:,i),histcEdges,1);
+    
+    %bin label should range from 1 to nbins(i)
     bin(:,i) = max(bin(:,i),1);
     bin(:,i) = min(bin(:,i),nbins(i));
 end
@@ -46,6 +72,19 @@ end
 % Combine the three vectors of 1D bin counts into a grid of 3D bin
 % Counts. A = accumarray(subs,val,sz) If sub = 1 and val = 101,realValue = val-1+sub = 101
 n = accumarray(bin,1,nbins);
+
+%{
+if(flag == 1)
+    for i = 1:nbins(1)
+        for j  = 1:nbins(2)
+            n(i,j) = sum(n1(i,j,:));
+        end
+    end
+else
+    n = n1;
+end
+%}
+
 a = (n>0);
 len = sum(a(:));
 
@@ -56,8 +95,9 @@ C = zeros(len,3);% Color
 
 counts = 1;
 
-if (flag == 0) 
-    Z = zeros(1,len);
+if isPlot
+    if (flag == 0) 
+        Z = zeros(1,len);
     
     for z = 1:nbins(3)
         for y = 1:nbins(2)
@@ -69,9 +109,9 @@ if (flag == 0)
                     S(counts) = n(x,y,z)/10;
             
                     % Using inverse function to calculate the original color
-                    R = uint8(1/2 * edges{1}(x)+ 1/3 * edges{2}(y) - 1/6 * edges{3}(z));
-                    G = uint8(-1/2 * edges{1}(x)+ 1/3 * edges{2}(y) - 1/6 * edges{3}(z));
-                    B = uint8(1/3 * edges{2}(y) + 1/3 * edges{3}(z));
+                    R = uint8(1/2 * ctrs{1}(x)+ 1/3 * ctrs{2}(y) - 1/6 * ctrs{3}(z));
+                    G = uint8(-1/2 * ctrs{1}(x)+ 1/3 * ctrs{2}(y) - 1/6 * ctrs{3}(z));
+                    B = uint8(1/3 * ctrs{2}(y) + 1/3 * ctrs{3}(z));
                     C(counts,:) = [im2double(R),im2double(G),im2double(B)];
                
                     counts = counts + 1;
@@ -80,16 +120,14 @@ if (flag == 0)
         end
     end 
 
-    %figure;
-    if isPlot
         scatter3(X,Y,Z,S,C,'filled','s');%,'MarkerfaceAlpha',0.8); 
 
         axis([0 16 0 8 0 16]);
 
-        xlabel(['rg axis [',num2str(round(edges{1}(1))),',',num2str(round(edges{1}(nbins(1)))),']']);
-        ylabel(['wb axis [',num2str(round(edges{2}(1))),',',num2str(round(edges{2}(nbins(2)))),']']);
-        zlabel(['by axis [',num2str(round(edges{3}(1))),',',num2str(round(edges{3}(nbins(3)))),']']);
-        title('3D histogram of three opponent color axes (No scaling)');
+        xlabel(['rg axis [',num2str(round(edges{1}(1))),',',num2str(round(edges{1}(nbins(1) + 1))),']']);
+        ylabel(['wb axis [',num2str(round(edges{2}(1))),',',num2str(round(edges{2}(nbins(2) + 1))),']']);
+        zlabel(['by axis [',num2str(round(edges{3}(1))),',',num2str(round(edges{3}(nbins(3) + 1))),']']);
+        title('3D histogram of three opponent color axes');
 
         box on
         ax = gca;
@@ -97,43 +135,47 @@ if (flag == 0)
         set(gca,'YDir','reverse'); 
 
         view(30,30);
-    end
+
 else
     for y = 1:nbins(2)
         for x = 1:nbins(1)
             if ~n(x,y) == 0
                 X(counts) = x;
                 Y(counts) = y;
-                S(counts) = n(x,y)/10;
+                S(counts) = n(x,y,:)/10;
             
-                j = 1;
                
                 % Because different colors may have same r' and g'
-                % Average color within each bin is used in here 
+                % Averaging the colors within each bin is used in here 
+                j = 1;
                 for i = 1:size(bin,1)
                     if(bin(i,1) == x && bin(i,2) == y)
+                        %record the pixels address
                         [ind(j,1),ind(j,2)] = ind2sub(size(I),i);
+                        %record the color of the pixel
+                        c(j,1) = I(ind(j,1),ind(j,2),1);
+                        c(j,2) = I(ind(j,1),ind(j,2),2);
+                        c(j,3) = I(ind(j,1),ind(j,2),3);
                         j = j + 1;
                     end
                 end
                 
                 if(size(ind,1)~= 1)
-                    c = uint8(mean(ind));
+                    c_ave = uint8(mean(c));
                 else% only one pixel within bin(x,y) 
-                    c = ind;
+                    c_ave = c;
                 end
-                C(counts,:)= im2double(uint8(I(c(1),c(2),:)));
+                C(counts,:)= im2double(uint8(c_ave));
                 
                 counts = counts + 1;
             end
         end
     end 
-    if isPlot
         scatter(X ,Y ,S,C,'filled','s','MarkerfaceAlpha',0.8); 
-        xlabel(['r'' axis [',num2str(edges{1}(1)),',',num2str(edges{1}(nbins(1))),']']);
-        ylabel(['g'' axis [',num2str(edges{2}(1)),',',num2str(edges{2}(nbins(2))),']']);
-
-        title('2D histogram of color constancy algorithm (No scaling)');
+        xlabel(['r'' axis [',num2str(edges{1}(1)),',',num2str(edges{1}(nbins(1) + 1)),']']);
+        ylabel(['g'' axis [',num2str(edges{2}(1)),',',num2str(edges{2}(nbins(2) + 1)),']']);
+        
+        title('2D histogram of color constancy algorithm');
     end
 end
 
