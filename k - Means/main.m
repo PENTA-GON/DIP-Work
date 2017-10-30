40defd4a56523073d28e5a9dd51b1d043c407f82
Aung Aung Phyo Wai <aung2phyowai@gmail.com>
2017-10-30 16:24:51 +0800

    *Added both test and train images of respective sift files *Modify main.m to show example of display sift features on top of image

--- k - Means/main.m
+++ k - Means/main.m
@@ -2,5 +2,11 @@ clear;close all;clc;
 
 addpath('DataSet(Paper3)');
 
-train_dir = './DataSet(Paper3)/bike-train-test-sift/train';
-test_dir = './DataSet(Paper3)/bike-train-test-sift/test';
+train_sift_dir = './DataSet(Paper3)/bike-train-test-sift/train';
+test_sift_dir = './DataSet(Paper3)/bike-train-test-sift/test';
+
+train_img_dir = './DataSet(Paper3)/bike-train-test-img/train';
+test_img_dir = './DataSet(Paper3)/bike-train-test-img/test';
+
+%Display SIFT features
+display_features(fullfile(train_sift_dir, 'bike_001.sift'), fullfile(train_img_dir, 'bike_001.bmp'), 0,0);
