d7b93c9ec484653be0e8a599f73752e92e341cef
PENTA-GON <32325746+PENTA-GON@users.noreply.github.com>
2017-11-03 00:30:42 +0800

    Update MeanShiftMain.m

--- Mean Shift/MeanShiftMain.m
+++ Mean Shift/MeanShiftMain.m
@@ -1,11 +1,19 @@
 clear, close all;
 
 %I = imread('mit.png');
-%I = rgb2gray(imread('cameraman.png'));
-I = imread('Monkey.png');
+I = imread('cameraman.png');
+%I = imread('Monkey.png');
+
+%I = rgb2gray(I);
+
+Hs = 8;
+Hr = 4;
+M = 40;
+wSize = 2;
 
 if size(I, 3) == 3
-    output = MeanShiftSeg(I, 8, 6, 4);
+    output = MeanShiftSegColor(I, Hs, Hr, M, wSize);
+    %output = MeanShiftSeg(I, Hs, Hr, M);
 else
-    output = MeanShiftSegGrayscale(I, 8, 6, 4);
+    output = MeanShiftSegGrayscale(I, Hs, Hr, M, wSize);
 end
