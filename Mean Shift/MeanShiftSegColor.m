f14fb7f732270ebc1512dab722c80f2cc41d71e1
PENTA-GON <32325746+PENTA-GON@users.noreply.github.com>
2017-11-03 00:06:12 +0800

    trials by Shubham; using Lab colorspace

--- Mean Shift/MeanShiftSegColor.m
+++ Mean Shift/MeanShiftSegColor.m
@@ -1,5 +1,8 @@
 function output = MeanShiftSeg(input, Hs, Hr, M, windowSize)
 
+org = input;
+input = rgb2lab(input);
+
 ht=size(input,1);
 wd=size(input,2);
 output=input;
@@ -12,7 +15,10 @@ modeY = [size_];
 
 clusterX = [size_];
 clusterY = [size_];
-clusterZ = [size_];
+
+clusterR = [size_];
+clusterG = [size_];
+clusterB = [size_];
 
 
 clusterSize = 0;
@@ -31,7 +37,9 @@ for y=1:ht
         % normalize with Hs & Hr
         newX = round(single(x) / Hs);
         newY = round(single(y) / Hs);
-        newZ = round(single(input(y,x)) / Hr);
+        newR = round(single(input(y,x,1)) / Hr);
+        newG = round(single(input(y,x,2)) / Hr);
+        newB = round(single(input(y,x,3)) / Hr);
         
         shiftX = x; % newX * Hs;
         shiftY = y; % newY * Hs;
@@ -49,26 +57,30 @@ for y=1:ht
         dist = 1;
         while(dist > 0.2)
             withinCount = 0;
-            accumulateZ = 0;
+            accumulateR = 0; accumulateG = 0; accumulateB = 0;
             accumulateX = 0; accumulateY = 0;
             for li=1:size(patch,1)
                 for lj=1:size(patch,2)
                     % normalize with Hs & Hr
                     yi = single(r1 + li - 1) / Hs;
                     xi = single(c1 + lj - 1) / Hs;
-                    xiZ = single(patch(li, lj)) / Hr;
+                    xiR = single(patch(li, lj, 1)) / Hr;
+                    xiG = single(patch(li, lj, 2)) / Hr;
+                    xiB = single(patch(li, lj, 3)) / Hr;
                     
-                    difZ = newZ - xiZ;
+                    difR = newR - xiR; difG = newG - xiG; difB = newB -xiB;
                     difX = newX - xi; difY = newY - yi;
                     
                     % Epanechnikov kernel derivative, basically only consider
                     % point in the hypersphere of radius bandwidth
-                    euclideanD = sqrt(difX*difX + difY*difY + difZ*difZ);
+                    euclideanD = sqrt(difX*difX + difY*difY + difR*difR + difG*difG + difB*difB);
                     
                     % If incribed hypersphere is the bound, then limit should be windowSize. However, if the superscribed
                     % hypershpere is the bound, then the limit should be distance from center to a vertex, i.e., sqrt(dim)*windowSize, where dim - dimesnions 
                     if euclideanD <= windowSize
-                        accumulateZ = accumulateZ + xiZ;
+                        accumulateR = accumulateR + xiR;
+                        accumulateG = accumulateG + xiG;
+                        accumulateB = accumulateB + xiB;
                         accumulateX = accumulateX + xi;
                         accumulateY = accumulateY + yi;
                         withinCount = withinCount + 1;
@@ -77,50 +89,64 @@ for y=1:ht
             end
             
             % get the mean of each channel
-            meanZ = accumulateZ / withinCount;
+            meanR = accumulateR / withinCount;
+            meanG = accumulateG / withinCount;
+            meanB = accumulateB / withinCount;
             meanX = accumulateX / withinCount;
             meanY = accumulateY / withinCount;
             
             % mean shift vec = mean - currentX
-            meanshiftVecZ = meanZ - newZ;
+            meanshiftVecR = meanR - newR;
+            meanshiftVecG = meanG - newG;
+            meanshiftVecB = meanB - newB;
             meanshiftVecX = meanX - newX;
             meanshiftVecY = meanY - newY;
             
             % compute the distance of the meanshift vec as terminating
             % condition
-            dist = sqrt(meanshiftVecZ*meanshiftVecZ + meanshiftVecX*meanshiftVecX + meanshiftVecY*meanshiftVecY);
+            dist = sqrt(meanshiftVecR*meanshiftVecR + meanshiftVecG*meanshiftVecG + meanshiftVecB*meanshiftVecB + meanshiftVecX*meanshiftVecX + meanshiftVecY*meanshiftVecY);
             
             % apply the mean shift vec
-            newZ = newZ + meanshiftVecZ;
+            newR = newR + meanshiftVecR;
+            newG = newG + meanshiftVecG;
+            newB = newB + meanshiftVecB;
             newX = newX + meanshiftVecX;
             newY = newY + meanshiftVecY;
         end
         
         % set the result of the mean shift as final color
-        output(y,x) = round(newZ);
+        output(y,x,1) = round(newR);
+        output(y,x,2) = round(newG);
+        output(y,x,3) = round(newB);
         modeX(y,x) = newX;
         modeY(y,x) = newY;
     end
 end
 
-subplot(2,2,2),imshow(output .* Hr); title('Meanshift Filtered');
+subplot(2,2,2),imshow(lab2rgb(output) .* Hr); title('Meanshift Filtered');
 
 % do the clustering
 for y=1:ht
     for x=1:wd
         
-        z = output(y,x);
+        r = output(y,x,1);
+        g = output(y,x,2);
+        b = output(y,x,3);
         
         % check if the result can be combine into a existing cluster
         clusterFound = false;
         for ci=1:clusterSize
-            cDifZ = single(clusterZ(ci) - z);
+            cDifR = single(clusterR(ci) - r);
+            cDifG = single(clusterG(ci) - g);
+            cDifB = single(clusterB(ci) - b);
             cDifX = clusterX(ci) - modeX(y,x);
             cDifY = clusterY(ci) - modeY(y,x);
             
-            cRDist = sqrt(cDifZ*cDifZ + cDifX*cDifX + cDifY*cDifY);
+            cRDist = sqrt(cDifR*cDifR + cDifG*cDifG + cDifB*cDifB + cDifX*cDifX + cDifY*cDifY);
             if cRDist <= 0.5
-                output(y,x,1) = clusterZ(ci) * Hr;
+                output(y,x,1) = clusterR(ci) * Hr;
+                output(y,x,2) = clusterG(ci) * Hr;
+                output(y,x,3) = clusterB(ci) * Hr;
                 
                 % store membership id of cluster
                 clusterMemberSize(ci) = clusterMemberSize(ci) + 1;
@@ -134,10 +160,14 @@ for y=1:ht
         
         if ~clusterFound
             % set the result of the mean shift as final color
-            output(y,x) = z * Hr;
+            output(y,x,1) = r * Hr;
+            output(y,x,2) = g * Hr;
+            output(y,x,3) = b * Hr;
             
             clusterSize = clusterSize + 1;
-            clusterZ(clusterSize) = z;
+            clusterR(clusterSize) = r;
+            clusterG(clusterSize) = g;
+            clusterB(clusterSize) = b;
             clusterX(clusterSize) = modeX(y,x);
             clusterY(clusterSize) = modeY(y,x);
             
@@ -152,7 +182,9 @@ end
 % merge the small cluster
 for ci=1:clusterSize
     if(clusterMemberSize(ci) < M)
-        z = clusterZ(ci);
+        r = clusterR(ci);
+        g = clusterG(ci);
+        b = clusterB(ci);
         x = clusterX(ci);
         y = clusterY(ci);
         
@@ -161,11 +193,13 @@ for ci=1:clusterSize
         nearestDist = 100000;
         for ci2=1:clusterSize
             if(ci ~= ci2 && clusterMemberSize(c2) ~= 0)
-                difZ = z - clusterZ(ci2);
+                difR = r - clusterR(ci2);
+                difG = g - clusterG(ci2);
+                difB = b - clusterB(ci2);
                 difX = x - clusterX(ci2);
                 difY = y - clusterY(ci2);
                 
-                euclidean = sqrt(difZ*difZ + difX*difX + difY*difY);
+                euclidean = sqrt(difR*difR + difG*difG + difB*difB + difX*difX + difY*difY);
                 % this cluster is close by
                 if(euclidean < nearestDist)
                     newCluster = ci2;
@@ -180,7 +214,9 @@ for ci=1:clusterSize
             memberY = clusterMember(ci, memberId, 2);
             
             if (memberX ~= 0 && memberY ~= 0)
-                output(memberY,memberX,1) = clusterZ(newCluster) * Hr;
+                output(memberY,memberX,1) = clusterR(newCluster) * Hr;
+                output(memberY,memberX,2) = clusterG(newCluster) * Hr;
+                output(memberY,memberX,3) = clusterB(newCluster) * Hr;
                 
                 % store membership id of cluster
                 clusterMemberSize(newCluster) = clusterMemberSize(newCluster) + 1;
@@ -197,7 +233,7 @@ end
     
 fprintf('\n time taken for meanshift=%f \n',toc);
 
-subplot(2,2,1),imshow(input); title('input image');
-subplot(2,2,3),imshow(output); title('meanshift segmented image')
+subplot(2,2,1),imshow(org); title('input image');
+subplot(2,2,3),imshow(lab2rgb(output)); title('meanshift segmented image')
 
 end
