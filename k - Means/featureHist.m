3eceb8b122cb2cc5c4c017177118a594f1d3824f
Aung Aung Phyo Wai <aung2phyowai@gmail.com>
2017-11-06 10:10:46 +0800

    Add relevant comments for building features histogram

--- k - Means/featureHist.m
+++ k - Means/featureHist.m
@@ -4,20 +4,22 @@ function [ featHist ] = featureHist( img, train_aver,k, isPlot)
 featHist = zeros(k,1);
 
 for i = 1:size(img,2)
-
+    %assign min distance 
     min = distance(img(:,i),train_aver(:,1),1);
     min_ind = 1;
-    
+    %Find min distance compared with other cluster centroids
     for j = 2:k
         dis = distance(img(:,i),train_aver(:,j),1);
         if(dis < min)
             min = dis;
             min_ind = j;
         end
-    end
+    end 
+    %Build features histogram of specified no of visual tokens
     featHist(min_ind) = featHist(min_ind) + 1;
 end
 
+%Plotting of Histogram
 if isPlot
     figure;
     set(gcf, 'Name', 'Histogram of visual tokens');
